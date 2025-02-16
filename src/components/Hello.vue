6b1bc2221f9e394a8db6fa01e766061392c64891
habibie <rizaldihabibie28@gmail.com>
2025-02-16 15:50:52 +0700

    feat: attributes

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,11 +1,15 @@
 <script setup>
     console.info('Hello guys')
     const helloHtml = "<h1>Hello VHtml</h1>";
+    const classHello = "hello";
+    const id = "hello";
+    const buttonDisabled = true;
 </script>
 
 <template>
-    <h1 class="hello">{{ helloHtml }}</h1>
+    <h1 :id :class ="classHello">{{ helloHtml }}</h1>
     <div v-html="helloHtml"></div>
+    <button :disabled="buttonDisabled">This is button</button>
 </template>
 
 <style scoped>
---
+++ src/components/HelloMultipleAttributes.vue
@@ -0,0 +1,19 @@
+<script setup>
+    console.info('Hello guys')
+    const helloHtml = "<h1>Hello VHtml</h1>";
+    const dataAttributes = {
+        id: "hello",
+        class: "hello"
+    }
+</script>
+
+<template>
+    <h1 v-bind="dataAttributes">{{ helloHtml }}</h1>
+    <div :class="data.class" v-html="helloHtml"></div>
+</template>
+
+<style scoped>
+    .hello {
+        color: green;
+    }
+</style>
