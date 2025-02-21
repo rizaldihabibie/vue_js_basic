733dc10245611afc65cce5f63b900ffdca296753
unknown <rizaldihabibie28@gmail.com>
2025-02-21 10:51:19 +0700

    feat: learn the basic of reactive state

---
+++ src/components/Counter.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {ref} from "vue";
+let count = ref(0);
+
+function increment() {
+    count.value++;
+}
+</script>
+<template>
+    <div>
+        <h1 id="count">Counter: {{ count }}</h1>
+        <button v-on:click="increment">Increment</button>
+    </div>
+</template>
